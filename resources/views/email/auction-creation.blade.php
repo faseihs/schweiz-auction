b4578a75c08087bdb4589380d80c8ff97741c96e
Faseih Saad <fsaad.bese15seecs@seecs.edu.pk>
2019-02-14 18:46:14 +0500

    Added Mail Creation and Some edits in admin panel

--- resources/views/email/auction-creation.blade.php
+++ resources/views/email/auction-creation.blade.php
@@ -1,6 +1,6 @@
 <h4>A new {{$auction->vehicle}} has been added</h4>
     <h4>We wish you a pleasant auction</h4>
-<a href="http://schweiz-auction/client/auction/{{$auction->id}}">
+<a href="http://auction.ambreenfatima.com/client/auction/{{$auction->id}}">
     <h4>Auction : {{$auction->title}}</h4>
 <img src="{{$message->embed(asset($auction->getThumbnail()))}}" alt="Image">
 </a>
--- resources/views/email/auction-winner.blade.php
+++ resources/views/email/auction-winner.blade.php
@@ -1,6 +1,6 @@
 <h4>{{$user->name}} has won the auction {{$auction->title}}</h4>
 
-<a href="http://schweiz-auction/client/auction/{{$auction->id}}">
+<a href="http://auction.ambreenfatima.com/client/auction/{{$auction->id}}">
     <h4>Auction : {{$auction->title}}</h4>
     <img src="{{$message->embed(asset($auction->getThumbnail()))}}" alt="Image">
 </a>
